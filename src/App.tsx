1878dcdb145d4170c2b274fa536eef48749bb77e
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-05-23 17:18:03 -0300

    entrega 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import api from "./api";
+
 function App() {
-  return <main>Let&apos;s get this party started</main>;
+  const [pokemon, setPokemon] = useState({})
+  const [showName, setShowName] = useState(false)
+  
+  useEffect(() => {
+    api.random().then(setPokemon)
+  }, [])
+  
+  return <main>
+      <h1>Quien es ese pokemon?</h1>
+      
+        <img
+        width='500px'
+        height='500px'
+        src={pokemon?.image} 
+        alt={pokemon?.name}
+        className={ showName ? '' : 'ocultar'}
+        />
+
+        <button
+          onClick={ ()=> setShowName(!showName) }
+        >Adivinar</button>
+
+        {
+          showName 
+          &&
+          <div>{ pokemon.name }</div>
+        }
+        
+  </main>;
 }
 
 export default App;
